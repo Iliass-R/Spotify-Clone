677f66eb0aaf5cf4104c62c9398eaa652e81d07e
Iliass-R <iliass.rami.pro@gmail.com>
2022-11-23 13:25:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<RegisterOrSignup />
+	</div>
+</template>
+
+<script setup>
+	import RegisterOrSignup from './components/RegisterOrSignup.vue';
+</script>
+
+<style>
+	@import url('http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext');
+
+	#app {
+		font-family: 'Lato', sans-serif;
+	}
+</style>
---
+++ src/components/RegisterOrSignup.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="h-screen bg-black">
+		<h2 class="text-red-400 m-64 text-5xl">Hello World</h2>
+		<p class="text-red-400">test</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style></style>
